9f3914deed4ac41e67859ecb9dff47bc7c965c62
PedroSchettini <pedro.sousa@germinare.org.br>
2024-05-20 10:36:09 -0300

    Subindo tipo de jogo

--- inicio/main.js
+++ inicio/main.js
@@ -1,4 +1,9 @@
-// const btnInicio = document.getElementById("botao_jogar")
+portainicio = document.querySelector("#botao_jogar")
+door = document.querySelector(".door")
+boxgameType = document.querySelector("#game_type")
+close = document.querySelector("#close")
+btnStudent = document.querySelector("#btn_student")
+btnAdventure = document.querySelector("#btn_adventure")
 // const loading = document.getElementById("loading")
 
 // loading.classList.add("invisivel")
@@ -11,7 +16,27 @@
 //     }, 1500)
 // })
 
-door = document.querySelector(".door")
+portainicio.addEventListener('click', () => {
+    boxgameType.classList.remove("invisivel")
+    boxgameType.classList.add("game_type_class")
+    close.classList.remove("invisivel")
+})
+
+close.addEventListener('click', () => {
+  close.classList.add("invisivel")
+  boxgameType.classList.add("invisivel")
+  boxgameType.classList.remove("game_type_class")
+})
+
+btnStudent.addEventListener('click', () => {
+    window.location.replace('../principal/index.html')
+    localStorage.setItem('gameType', 1)
+})
+
+btnAdventure.addEventListener('click', () => {
+  window.location.replace('../principal/index.html')
+  localStorage.setItem('gameType', 2)
+})
 
 door.addEventListener('mouseleave', () => {
     door.classList.add('closing')
---
+++ principal/main.js
@@ -0,0 +1,15 @@
+const gameType = localStorage.getItem('gameType')
+
+fase1 = document.querySelector('#fase1')
+fase2 = document.querySelector('#fase2')
+fase3 = document.querySelector('#fase3')
+fase4 = document.querySelector('#fase4')
+fase5 = document.querySelector('#fase5')
+fase6 = document.querySelector('#fase6')
+
+if(gameType == 1){
+
+}
+else if(gameType == 2){
+
+}
