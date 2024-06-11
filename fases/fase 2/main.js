07c5681552d571e3ebf61b443348ae4f592900e8
GeoRPOliveira <geovanna.oliveira@germinare.org.br>
2024-06-11 12:52:35 -0300

    Fase 2 completa

--- fases/fase 2/main.js
+++ fases/fase 2/main.js
@@ -5,6 +5,7 @@ const step4 = document.querySelector("#step4")
 const btn_next_step1 = document.querySelector('#btn_next_step1')
 const btn_next_step2 = document.querySelector('#btn_next_step2')
 const btn_next_step3 = document.querySelector('#btn_next_step3')
+const btn_next_step_final = document.querySelector('#btn_next_step_final')
 
 btn_next_step1.addEventListener('click', () => {
     step1.classList.add('invisible')
@@ -22,4 +23,15 @@ btn_next_step3.addEventListener('click', () => {
     step2.classList.add('invisible')
     step3.classList.add('invisible')
     step4.classList.remove('invisible')
+})
+
+btn_next_step_final.addEventListener('click', () => {
+    let gametype = localStorage.getItem('gameType')
+    let phases = localStorage.getItem('phases')
+    phases = JSON.parse(phases)
+    if(gametype == '1'){
+        phases.push(2)
+        localStorage.setItem('phases', JSON.stringify(phases))
+    }
+    window.location.replace('./../../principal/index.html')
 })
