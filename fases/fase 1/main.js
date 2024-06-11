c5a86aa2117353aaad0ec2eba9e1ee9eea888759
PedroSchettini <pedro.sousa@germinare.org.br>
2024-06-11 12:54:39 -0300

    Subindo liberação de fases e quiz

--- fases/fase 1/main.js
+++ fases/fase 1/main.js
@@ -86,7 +86,7 @@ btn_next_step10.addEventListener('click', () => {
     let phases = localStorage.getItem('phases')
     phases = JSON.parse(phases)
     if(gametype == '1'){
-        if(phases.indexOf(2) != -1){
+        if(phases.indexOf(2) == -1){
             phases.push(2)
             localStorage.setItem('phases', JSON.stringify(phases))
         }
